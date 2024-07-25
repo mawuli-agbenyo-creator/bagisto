fb712a965e2a314708c5c00f6475410931299dec
Mawuli Agbenyo <mawulikofiagbenyo@gmail.com>
2024-07-25 15:54:47 +0000

    chore: Update Paystack package configuration and dependencies

--- packages/Webkul/Paystack/src/Providers/PaystackServiceProvider.php
+++ packages/Webkul/Paystack/src/Providers/PaystackServiceProvider.php
@@ -23,14 +23,14 @@ class PaystackServiceProvider extends ServiceProvider
      */
     protected function registerConfig()
     {
-        // $this->mergeConfigFrom(
-        //     dirname(__DIR__) . '/Config/paymentmethods.php',
-        //     'payment_methods'
-        // );
-
         $this->mergeConfigFrom(
-            dirname(__DIR__) . '/Config/system.php',
-            'core'
+            dirname(__DIR__) . '/Config/paymentmethods.php',
+            'payment_methods'
         );
+
+        // $this->mergeConfigFrom(
+        //     dirname(__DIR__) . '/Config/system.php',
+        //     'core'
+        // );
     }
 }
