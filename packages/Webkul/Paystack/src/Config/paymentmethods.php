7f290fe14f145905a81ee306501dc24841a8756b
Mawuli Agbenyo <mawulikofiagbenyo@gmail.com>
2024-07-25 12:31:13 +0000

    chore: Update Paystack package configuration and dependencies

--- packages/Webkul/Paystack/src/Config/paymentmethods.php
+++ packages/Webkul/Paystack/src/Config/paymentmethods.php
@@ -4,6 +4,7 @@ return [
     'Paystack'  => [
         'code'        => 'Paystack',
         'title'       => 'Paystack',
+        'info'       => 'Paystack',
         'description' => 'Paystack',
         'class'       => 'Webkul\Paystack\Payment\Paystack',
         'active'      => true,
