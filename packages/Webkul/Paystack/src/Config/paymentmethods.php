a5823c1ca2ff0233896b8618cb6eeeb8dbe64923
Mawuli Agbenyo <mawulikofiagbenyo@gmail.com>
2024-07-25 17:15:44 +0000

    Update Paystack package configuration and dependencies

--- packages/Webkul/Paystack/src/Config/paymentmethods.php
+++ packages/Webkul/Paystack/src/Config/paymentmethods.php
@@ -9,5 +9,8 @@ return [
         'class'       => 'Webkul\Paystack\payment\Paystack',
         'active'      => true,
         'sort'        => 1,
+        'sandbox' => true,
+        'public_key' => env('PAYSTACK_PUBLIC_KEY'),
+        'secret_key' => env('PAYSTACK_SECRET_KEY'),
     ],
 ];
