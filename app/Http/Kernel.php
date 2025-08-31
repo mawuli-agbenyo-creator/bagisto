642cb0cb695512034608d3f9c73bdcdcd20158a1
Mawuli Agbenyo <mawulikofiagbenyo@gmail.com>
2025-08-31 23:35:11 +0000

    `Removed and commented out \Illuminate\Session\Middleware\StartSession::class in Kernel.php`

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends HttpKernel
         \Webkul\Installer\Http\Middleware\CanInstall::class,
 
         \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-        \Illuminate\Session\Middleware\StartSession::class,
+        // \Illuminate\Session\Middleware\StartSession::class,
     ];
 
     /**
@@ -36,7 +36,7 @@ class Kernel extends HttpKernel
         'web' => [
             \App\Http\Middleware\EncryptCookies::class,
             // \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            // \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
